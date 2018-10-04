fef008faa69faa216b3534a47c0aa30ec2d41a50
Matias Trujillo <mtrujillo@outlook.cl>
2018-10-04 16:41:37 -0300

    update version atomico cdn

--- bone.config.js
+++ bone.config.js
@@ -18,6 +18,7 @@ export default {
         }
     ],
     onSubmit(data) {
+        data.__version__ = "0.3.3";
         data.$source = data.__typecss__;
         data.$dist = data.__name__ = data.__name__.replace(/[^\w]+/g, "-");
         return data;
